df1539df5d3b3f4c80db195644080e195e72aa6e
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-07-25 10:08:40 -0300

    Manipulando elemento no DOM

---
+++ eventoDom/main.js
@@ -0,0 +1,6 @@
+const element = document.querySelector('#calcular');
+const result = document.querySelector('.resultado');
+
+element.addEventListener('click', () => {
+  element.innerHTML = "fui clicado"
+})
